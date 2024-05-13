0364d967ee8c7df1f8e74bd069822ff59cdbc2b5
mcfsw <145933511+mcfsw@users.noreply.github.com>
2024-05-14 00:08:59 +0200

    f

---
+++ website2/js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-mc-fractal-studies-c-2017","displayName":"MC Fractal Studies (c) 2017","link":{"linkType":"LinkTypePage","href":"#!page-mc-fractal-studies-c-2017"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
